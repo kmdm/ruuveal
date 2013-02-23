c5e23b764d82dc041f0bae57ec182c1870dabe3c
Kenny Millington <kenny@kennynet.co.uk>
2013-02-23 16:40:51 +0000

    Add updated header missing from last commit

--- htczip.h
+++ htczip.h
@@ -3,16 +3,15 @@
 #define HTC_ZIP_HEADER_DEFAULT_CHUNKS 0x0A
 #define HTC_ZIP_HEADER_DEFAULT_KEYMAP 0xA4
 #define HTC_ZIP_HEADER_DEFAULT_MAINVER "0.00.0000.0"
-#define HTC_ZIP_HEADER_MAINVER_SIZE 11
+#define HTC_ZIP_HEADER_MAINVER_SIZE 20
 typedef struct {
     char magic[8];
     unsigned short keymap_index;
     unsigned char chunks;
-    char padding1[9];
     char mainver[HTC_ZIP_HEADER_MAINVER_SIZE];
-    char padding2[1];
+    char padding1[1];
     unsigned int unknown;
-    char padding3[92];
+    char padding2[92];
 } htc_zip_header_t;
 
 int htc_zip_init_header(htc_zip_header_t *);
